f620b971f38a21c78e768084f20bfa9ae837ba0f
Shuhei Nomura <info@danmaq.com>
2017-08-02 16:43:11 +0900

    Created Hello, world!

--- App.js
+++ App.js
@@ -1,23 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+export default class HelloWorldApp extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    return (<Text>Hello, world!</Text>);
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('netch-native', () => HelloWorldApp);
--- App.test.js
+++ App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import App from './App';
+import HelloWorldApp from './App';
 
 import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON();
+  const rendered = renderer.create(<HelloWorldApp />).toJSON();
   expect(rendered).toBeTruthy();
 });
