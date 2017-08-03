e480df63f063e8046b078431806c8b05b698455b
Shuhei Nomura <info@danmaq.com>
2017-08-03 11:32:43 +0900

    Tutorial: Learned state and implemented blink view using state.

--- App.js
+++ App.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { AppRegistry, Image, Text, View } from 'react-native';
 
 class Greeting extends React.Component {
+  constructor(props) {
+    super(props);
+    this.text = { "showText": true };
+    setInterval(() => {
+      this.setState(p => ({ "showText": (p ? !p["showText"] : true) }));
+    }, 1500);
+  }
   render() {
+    const state = this.state;
+    const str = state && state.showText ? this.props.name : '■■■■■■';
     return (
-      <Text>Hello {this.props.name}!</Text>
+      <Text>Hello {str}!</Text>
     );
   }
 }
@@ -13,9 +22,10 @@ export default class HelloWorldApp extends React.Component {
   render() {
     return (
       <View style={{alignItems: 'center'}}>
-        <Greeting name="Enna" />
-        <Greeting name="Grace" />
-        <Greeting name="Ark" />
+        <Greeting name="Enna Braun" />
+        <Greeting name="Grace Braun" />
+        <Greeting name="Ark Wimmer" />
+        <Greeting name="Gordon Bollmann" />
       </View>
     );
   }
