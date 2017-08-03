d3d915b253f7a1a7b8c83324124eaef2d8751f44
Shuhei Nomura <info@danmaq.com>
2017-08-03 16:20:32 +0900

    Tutorial: Learned input UI and reaction.

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { AppRegistry, View } from 'react-native';
+import { AppRegistry, View, Text, TextInput } from 'react-native';
 
 export default class HelloWorldApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
   render() {
     return (
-      <View style={{
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        backgroundColor: 'cornflowerblue'}}>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      <View style={{ padding: 10 }}>
+        <TextInput style={{ padding: 10, fontSize: 36 }}
+                   placeholder="Type here to 🐹."
+                   onChangeText={t => this.setState({text: t})} />
+        <Text style={{ padding: 10, fontSize: 42 }}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
       </View>
     );
   }
