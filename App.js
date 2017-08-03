43c232da57f2746b69b0eb520383b2390b304692
Shuhei Nomura <info@danmaq.com>
2017-08-03 19:08:33 +0900

    Tutorial: Learned Touchable and Long Tap.

--- App.js
+++ App.js
@@ -1,43 +1,65 @@
 import React from 'react';
-import { AppRegistry, View, Button, StyleSheet, Alert } from 'react-native';
+import { AppRegistry, Alert, Button, StyleSheet, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, Text, View, Platform } from 'react-native';
 
 export default class HelloWorldApp extends React.Component {
   static _onPressButton() {
     Alert.alert("May I help you?");
   }
+  static _onLongPressButton() {
+    Alert.alert("Long Tapped.");
+  }
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.container}>
-          <Button onPress={HelloWorldApp._onPressButton} title="HIT ME." />
-        </View>
-        <View style={styles.container}>
-          <Button onPress={HelloWorldApp._onPressButton} title="HIT ME." color="#841584" />
-        </View>
-        <View style={styles.alternativeLayoutButtonContainer}>
-          <Button onPress={HelloWorldApp._onPressButton} title="This looks great!" />
-        </View>
-        <View style={styles.alternativeLayoutButtonContainer}>
-          <Button onPress={HelloWorldApp._onPressButton} title="HIT ME." color="#841584" />
-        </View>
+        <TouchableHighlight onPress={HelloWorldApp._onPressButton} underlayColor="white">
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableHighlight</Text>
+          </View>
+        </TouchableHighlight>
+        <TouchableOpacity onPress={HelloWorldApp._onPressButton}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableOpacity</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableNativeFeedback onPress={HelloWorldApp._onPressButton}
+                                 background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : ''}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableNativeFeedback</Text>
+          </View>
+        </TouchableNativeFeedback>
+        <TouchableWithoutFeedback onPress={HelloWorldApp._onPressButton}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableWithoutFeedback</Text>
+          </View>
+        </TouchableWithoutFeedback>
+        <TouchableHighlight onPress={HelloWorldApp._onPressButton}
+                            onLongPress={HelloWorldApp._onLongPressButton}
+                            underlayColor="white">
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Available Long Press</Text>
+          </View>
+        </TouchableHighlight>
       </View>
     );
   }
 }
 
-const styles =
-  StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-    },
-    buttonContainer: { margin: 20 },
-    alternativeLayoutButtonContainer: {
-      margin: 20,
-      flexDirection: 'row',
-      justifyContent: 'space-between'
-    }
-  });
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 60,
+    alignItems: 'center'
+  },
+  button: {
+    marginBottom: 30,
+    width: 260,
+    alignItems: 'center',
+    backgroundColor: '#2196F3'
+  },
+  buttonText: {
+    padding: 20,
+    color: 'white'
+  }
+})
 
 // skip this line if using Create React Native App
 AppRegistry.registerComponent('netch-native', () => HelloWorldApp);
