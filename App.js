ceabf1c2da099cf8a29af0d01b6448b7e800442b
Shuhei Nomura <info@danmaq.com>
2017-08-04 18:33:23 +0900

    Revert "Tutorial: Learned SectionView."
    This reverts commit 756fa13250c6491513c026592665e7289ff33fed.

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { AppRegistry, SectionList, Text, View, StyleSheet } from 'react-native';
+import { AppRegistry, FlatList, Text, View, StyleSheet } from 'react-native';
 
 export default class HelloWorldApp extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <SectionList sections={[
-            {title: 'D', data: ['Devin']},
-            {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
-          ]}
-                     renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
-                     renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>} />
+        <FlatList data={[{key: "Enna"}, {key: "Grace"}, {key: "Arc"}, {key: "Bollmann"}]}
+                  renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>} />
       </View>
     );
   }
@@ -20,15 +16,6 @@ const styles = StyleSheet.create({
   container: {
    flex: 1,
    paddingTop: 22
-  },
-  sectionHeader: {
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
-    fontSize: 14,
-    fontWeight: 'bold',
-    backgroundColor: 'rgba(247,247,247,1.0)',
   },
   item: {
     padding: 10,
