720985bf1801738cbdb81534a6a5990dc6a71751
Shuhei Nomura <info@danmaq.com>
2017-08-04 14:00:45 +0900

    Tutorial: Learned FlatList

--- App.js
+++ App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import { AppRegistry, Image, Text, ScrollView } from 'react-native';
+import { AppRegistry, FlatList, Text, View, StyleSheet } from 'react-native';
 
 export default class HelloWorldApp extends React.Component {
   render() {
     return (
-      <ScrollView>
-        <Text style={{fontSize:96}}>Scroll me</Text>
-        <Image source={{uri: "https://danmaq.com/enna.png"}} style={{width: 256, height: 256}} />
-        <Image source={{uri: "https://danmaq.com/wp-content/uploads/2012/11/em_POP.png"}} style={{width: 256, height: 256}} />
-        <Image source={{uri: "https://danmaq.com/enna.png"}} style={{width: 256, height: 256}} />
-        <Image source={{uri: "https://danmaq.com/wp-content/uploads/2012/11/em_POP.png"}} style={{width: 256, height: 256}} />
-        <Image source={{uri: "https://danmaq.com/enna.png"}} style={{width: 256, height: 256}} />
-        <Text style={{fontSize:96}}>Scroll me</Text>
-        <Image source={{uri: "https://danmaq.com/enna.png"}} style={{width: 256, height: 256}} />
-        <Image source={{uri: "https://danmaq.com/wp-content/uploads/2012/11/em_POP.png"}} style={{width: 256, height: 256}} />
-        <Image source={{uri: "https://danmaq.com/enna.png"}} style={{width: 256, height: 256}} />
-        <Image source={{uri: "https://danmaq.com/wp-content/uploads/2012/11/em_POP.png"}} style={{width: 256, height: 256}} />
-        <Image source={{uri: "https://danmaq.com/enna.png"}} style={{width: 256, height: 256}} />
-      </ScrollView>
+      <View style={styles.container}>
+        <FlatList data={[{key: "Enna"}, {key: "Grace"}, {key: "Arc"}, {key: "Bollmann"}]}
+                  renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>} />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});
 
 // skip this line if using Create React Native App
 AppRegistry.registerComponent('netch-native', () => HelloWorldApp);
